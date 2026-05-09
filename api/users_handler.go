be274782fa66e6602b139965543c57349546af70
zealot <zealot@theplant.jp>
2026-05-09 08:58:21 +0800

    fix(api): set explicit Content-Type on ListUsers

--- api/users_handler.go
+++ api/users_handler.go
@@ -6,5 +6,6 @@ import (
 )
 
 func ListUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "applicaton/json")
 	json.NewEncoder(w).Encode(map[string]any{"users": []string{}})
 }
