a7c83e2f65e9344f4ef4a7be30ee1588dd964be2
zealot <zealot@theplant.jp>
2026-05-09 08:56:25 +0800

    feat(api): paginate ListUsers via ?limit query param

--- api/users_handler.go
+++ api/users_handler.go
@@ -3,8 +3,16 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
 func ListUsers(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(map[string]any{"users": []string{}})
+	limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
+	if limit <= 0 || limit > 100 {
+		limit = 20
+	}
+	json.NewEncoder(w).Encode(map[string]any{
+		"users": []string{},
+		"limit": limit,
+	})
 }
